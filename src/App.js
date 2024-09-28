c63dad62a0155d8735138c5e3dcafad9b69b2cd8
PhamThanhSu <psu95228@gmail.com>
2024-09-28 19:23:58 +0700

    cap nhat reactapp routes chuyen trang

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import './App.scss';
 import Header from './components/Header/Header';
-import { Link } from "react-router-dom";
-import logo from './logo.svg';
+import { Outlet } from "react-router-dom";
+import './App.scss';
 
 const App = () => {
 
   return (
     <div className="app-container">
-      <Header />
-      <div>
-        test Link
-        <div>
-          <button>
-            <Link to="/users">go to user page</Link>
-          </button>
-          <button>
-            <Link to="/admins">go to admin page</Link>
-          </button>
+      <div className='header-container'>
+        <Header />
+      </div>
+      <div className='main-container'>
+        <div className='sidenav-container'>
+        </div>
+        <div className='app-content'>
+          <Outlet />
         </div>
       </div>
     </div>
--- src/index.js
+++ src/index.js
@@ -12,6 +12,8 @@ import {
 } from "react-router-dom";
 import User from './components/User/User';
 import Admin from './components/Admin/Admin';
+import HomePage from './components/Home/HomePage';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -19,9 +21,11 @@ root.render(
     {/* <React.StrictMode> */}
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="users" element={<User />} />
-        <Route path="admins" element={<Admin />} />
+        <Route path="/" element={<App />} >
+          <Route index element={<HomePage />} />
+          <Route path="users" element={<User />} />
+          <Route path="admins" element={<Admin />} />
+        </Route>
       </Routes>
     </BrowserRouter>
     {/* </React.StrictMode> */}
