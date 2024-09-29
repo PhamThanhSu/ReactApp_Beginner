9ef6bac4b4ad64c1854f79c9d014e57d99b2767e
PhamThanhSu <psu95228@gmail.com>
2024-09-29 22:32:08 +0700

    cap nhat sidebar va css

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,6 +1,19 @@
+import SideBar from "./SideBar";
+import './Admin.scss';
+import { FaHeart, FaBars } from 'react-icons/fa';
+import { useState } from "react";
 const Admin = (props) => {
+    const [collapsed, setcollapsed] = useState(false);
     return (
-        <div> Admin component</div>
+        <div className="admin-container">
+            <div className="admin-sidebar">
+                <SideBar collapsed = {collapsed}/>
+            </div>
+            <div className="admin-content">
+                <FaBars onClick={() => setcollapsed(!collapsed)}/>
+                content go here
+            </div>
+        </div>
     )
 }
 export default Admin;
